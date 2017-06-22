941ae4f9e16a8b5edfc0f1b8c16931c9e3162e7e
Josep Viciana <josep.viciana@3data.net>
2017-06-22 10:37:08 +0200

    Calculate fees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +12,55 @@ export class AppComponent {
     originalRemainingMonths: number = 480;
     tae: number = 2.7;
     startDate = new Date();
+    fees: Array<any> = [];
     
-    interestsTotal: number = 0;
-    pendingCapital: number = this.originalRemainingCapital;
-    
+    working: boolean = false;
     
     ngOnInit(){
+        // this.calculateFees();
+    }
+    
+    ngAfterViewInit() {
+        // this.calculateFees();
+    }
+
+    
+    calculateFees(){
+                
+        this.working = true;
+        this.fees = [];
         
-        let month: number = 1;
+        let originalRemainingMonths = this.originalRemainingMonths;
+        let originalRemainingCapital = this.originalRemainingCapital;
+        let tae = this.tae;
+        let interestsTotal: number = 0;
+        let pendingCapital: number = this.originalRemainingCapital;
+        let month: number = 0;
         let year: number = 0;
 
-        console.log(Math.round(this.pendingCapital * 100) / 100);
+        while( Math.round(pendingCapital * 100) / 100 > 0 && month <= originalRemainingMonths){
+            let fee             = Math.round((pendingCapital * (tae/12) / (100 * ( 1 - Math.pow(1 + (tae/12) / 100, (month-originalRemainingMonths))))) * 100) / 100;
+            let interests       = Math.round((pendingCapital * (tae/12) / 100) * 100) / 100;
+            let amortization    = fee - interests;
+            month++;
+            interestsTotal  += interests;
+            pendingCapital  -= amortization;
+            
+            this.fees.push({
+                originalRemainingMonths: originalRemainingMonths,
+                originalRemainingCapital: originalRemainingCapital,
+                month: month,
+                year: Math.floor((month-1) / 12),
+                fee: fee,
+                tae: this.tae,
+                interests: interests,
+                interestsTotal: interestsTotal,
+                pendingCapital: pendingCapital,
+                amortization: amortization
+            });
+        }
         
-        // while( Math.round(this.pendingCapital * 100) / 100 > 0 && month <= this.originalRemainingMonths ){
-        //     let fee             = Math.round((this.pendingCapital * (this.tae/12) / (100 * ( 1 - Math.pow(1 + (this.tae/12) / 100, (month-this.originalRemainingMonths))))) * 100) / 100;
-        //     let interests       = Math.round((this.pendingCapital * (this.tae/12) / 100) * 100) / 100;
-        //     let amortization    = fee - interests;
-        //     this.interestsTotal  += interests;
-        //     this.pendingCapital  -= amortization;
-        // }
-        
+        this.working = false;
     }
     
     
